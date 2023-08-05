180596c97bd0c331bbf43ab585aa8a773a1f5bda
rahoc369782 <rahuldarekar273@gmail.com>
2023-08-05 17:57:50 +0530

    code updated for searching and also flexibility to add to node in start or end is added

--- linked_list.c
+++ linked_list.c
@@ -16,10 +16,13 @@ Desc : Implementation of linkedlist using c structures and pointers
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #define PRINT 'p'
-#define NODE 'n'
+#define INSERT 'i'
+#define FIND 'f'
 #define DELETE 'd'
 #define EXIT 'e'
+
 #define TRACE { printf("Linked list is initiated...\nAt moment %s %s \n",__DATE__ , __TIME__); }
 
 
@@ -47,10 +50,9 @@ struct node {
 
 
 struct list {
-	struct node* list_init; // Initially NULL;
+	struct node* h; // Head of LIST;
+	struct node* t; // Tail of LIST;
 	int  list_len; // Initially declare as 0;
-	short a;
-	char h;
 };  
 
 struct list* _list; // Declare global list pointer
@@ -59,7 +61,7 @@ struct list* _list; // Declare global list pointer
 void print_list() {
 	// Print nodes of list through
 	list_node* current_node;
-	current_node = _list -> list_init;
+	current_node = _list -> h;
 	printf("\n-----------------------\n");
 	while(current_node != NULL) {
 		// Traverse the list
@@ -78,28 +80,54 @@ Search for node
 
 */
 
+list_node* search_node(void) {
+	int n; // Overwriting this memory each time
+	printf("Enter node value you want to search : ");
+	scanf("%d",&n);
+	fflush(stdin);
+	
+	list_node* curr = _list -> h;
+	printf("Main list addr %p",curr);
+	while(curr != NULL && n != curr -> node_data -> age) {
+		// while n not matches with age and curr is there loop through it.
+		curr = curr -> next;
+	}
+	printf("Found node value is %p",curr);
+	return curr;
+}
+
 void add_node(list_node *node) {
 	
 	// Operation will execute on list location (pointer);
+	char pos;
+	printf("Where you want to add start or end? default to (start) : ");
+	scanf("%c",&pos);
 	
-	list_node* currentnode = _list -> list_init;
-	if(currentnode == NULL) {
+	list_node* head = _list -> h;
+	list_node* tail = _list -> t;
+	if(head == NULL) {
 		// Head node of List
-		_list -> list_init = node;
+		_list -> h = node;
+		_list -> t = node;
 		_list -> list_len++;
 	} else {
-		node -> next = currentnode;
-		_list -> list_init = node;
-		_list -> list_len++;
+		if(pos == 's') {
+			node -> next = head;
+			_list -> h = node;
+			_list -> list_len++;
+		} else {
+			tail -> next = node;
+			_list -> t = node;
+			_list -> list_len++;
+		}
 	}
 }
-
 
 void delete_node(void) {
 	int n;
 	printf("Enter node value you want to delete : ");
 	scanf("%d",&n);
-	list_node* curr = _list -> list_init; // starting pointer of list
+	list_node* curr = _list -> h; // starting pointer of list
 	list_node* pre = NULL;
 	while(curr != NULL) {
 		if(n == curr -> node_data -> age) {
@@ -109,7 +137,7 @@ void delete_node(void) {
 			
 			if(pre == NULL) {
 				// first node of list is matched and need to delete
-				_list -> list_init = curr -> next;
+				_list -> h = curr -> next;
 			} else {
 				pre -> next = curr -> next;
 				free(curr -> node_data);
@@ -123,21 +151,21 @@ void delete_node(void) {
 	}
 }
 
-// node1 -> node2 -> node3
+
 
 list_node* create_list_node(list_node_data *n) {
 	/* 
 	   Initiate new node in list 
 	   With use of malloc 
 	*/
-	list_node *node = malloc(sizeof(list_node));	
+	list_node *node = malloc(sizeof(list_node)); // 16 bytes
 	node -> node_data = n;
 	node -> next = NULL;
 	return node;
 }
 
 void add_node_process(void) {
-	int i_age;
+	int i_age; // Overwriting this memory each time
 	printf("Enter node value you want to create : ");
 	scanf("%d",&i_age);
 	getchar();
@@ -153,7 +181,7 @@ void add_node_process(void) {
 
 void add_node_process_test(int i_age) {
 	// int i_age;
-	list_node_data *n = malloc(sizeof(list_node_data));
+	list_node_data *n = malloc(sizeof(list_node_data)); // 16 bytes
 	n -> age = i_age;
 	n -> n = NULL;
 	list_node *node = create_list_node(n); // Return pointer to newly created list node 
@@ -162,17 +190,19 @@ void add_node_process_test(int i_age) {
 
 int main() {
 	_list = malloc(sizeof(*_list));
-	_list -> list_init = NULL;
+	_list -> h = NULL;
 	_list -> list_len = 0;
 	TRACE
+	/*
 	clock_t t;
     t = clock();
-	for(int i = 0;i < 100000000; i++) {
+	for(int i = 0;i < 10; i++) {
 		add_node_process_test(i);
 	};
 	t = clock() - t;
     double time_taken = ((double)t)/CLOCKS_PER_SEC;
 	printf("Time took %f seconds to execute \n", time_taken);
+	*/
 	 // return 0;
 	while (1) {
 		char cmd;
@@ -184,9 +214,13 @@ int main() {
 					printf("Main list is printed below...\n");
 					print_list();
 					break;
-			case NODE:
+			case INSERT:
 					add_node_process();
 					printf("Node added successfully.\n");
+					break;
+		    case FIND:
+					search_node();
+					printf("Node Found successfully.\n");
 					break;
 			case DELETE:
 					delete_node();
@@ -200,7 +234,7 @@ int main() {
 					break;
 		};
 		
-		printf("Length of list is %d\n" , _list -> list_len);
+		printf("Length of list is %d\n" , _list -> list_len );
 	}
 	
 	return 0;
